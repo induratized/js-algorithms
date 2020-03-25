bdd8527c93605aa8e5a69b21eaa3df777b953c42
Abhinav Sharma <a.sharma.nitk@gmail.com>
2020-03-25 19:51:02 +0000

    js implementation

---
+++ sorting/selection-sort.js
@@ -0,0 +1,28 @@
+const unsorted = [1,234,21,435,543,5,09,3,345]
+// const unsorted = [8,4,6,2,4,3,2]
+
+
+function selectionSort(unsorted) {
+  for(let i = 0; i < unsorted.length - 1; i++) {
+    let minIndex = i;
+    for(let j = i+1; j < unsorted.length; j++) {
+      if(unsorted[j] < unsorted[minIndex]) {
+        minIndex = j;
+      }
+    }
+    
+    if(unsorted[i] > unsorted[minIndex]) {
+      doTheSwap(unsorted, i, minIndex);
+    }
+  }
+  
+  return unsorted;
+}
+
+function doTheSwap(array, iLeft, iRight) {
+  let temp = array[iLeft];
+  array[iLeft] = array[iRight];
+  array[iRight] = temp;
+}
+
+console.log(selectionSort(unsorted));
